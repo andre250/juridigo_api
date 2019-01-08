b4f6356a34235542d19861d6b292050270e64670
GuilhermeCaruso <guilherme.caruso@owldocs.com>
2019-01-07 23:40:04 -0200

    ajuste main.go

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"sync"
 
 	"github.com/juridigo/juridigo_api/config"
+	"github.com/juridigo/juridigo_api/helpers"
 	"github.com/juridigo/juridigo_api/models"
 	"github.com/juridigo/juridigo_api/routes"
 )
@@ -25,10 +26,10 @@ func main() {
 	// Obtenção das configurações de ambiente
 	configGlobal = config.GetConfig()
 	// inicialização da conexão
-	// helpers.ConDB()
+	helpers.Connection()
 	// Inicialização do servidor
 	serverConfig := []string{":", configGlobal.App.Port}
-	fmt.Printf("Mica [User] v%s ouvindo porta: %s", configGlobal.Version, configGlobal.App.Port)
+	fmt.Printf("Juridigo [User] v%s ouvindo porta: %s", configGlobal.Version, configGlobal.App.Port)
 	if http.ListenAndServe(strings.Join(serverConfig, ""), nil) != nil {
 		fmt.Println("Porta já esta sendo utilizada")
 		log.Fatal()
